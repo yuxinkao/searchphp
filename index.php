477946cae6d83ea459e6624919f9b6fb9e736fff
yuxinkao <99954219+yuxinkao@users.noreply.github.com>
2022-03-21 00:21:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,32 +11,6 @@ $link = mysqli_connect("acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.co
         or die("無法開啟MySQL資料庫連接!<br/>");
 mysqli_select_db($link, "gk4xqozmcqv07zee");  // 選擇gk4xqozmcqv07zeev資料庫
 // 查詢台式
-$sql = "SELECT date,kind,food,pay,place,dmoney,takemoney,people,inviteurl FROM delivery where kind='台式'";
-// 送出查詢的SQL指令
-if ( $result = mysqli_query($link, $sql) ) { 
-   // 取得記錄數
-   $total_records = mysqli_num_rows($result);
-   echo "項目類別:台式<br/>"; 
-   echo "開團總數:共 $total_records 團<br/>"; 
-   echo "<table border=><tr>";
-// 顯示欄位名稱
-while ( $meta = mysqli_fetch_field($result) )
-
-   echo "<td>".$meta->name."</td>";
-echo "</tr>"; // 取得欄位數
-$total_fields = mysqli_num_fields($result);
-// 顯示每一筆記錄
-while ($row = mysqli_fetch_row($result)) {
-   echo "<tr>"; // 顯示每一筆記錄的欄位值
-   for ( $i = 0; $i <= $total_fields-1; $i++ )
-      echo "<td>" . $row[$i] . "</td>";
-   echo "</tr>";
-}
-echo "</table>";
-
-else(echo"查無資料,快去開起第一團吧";)
-        
-// 查詢小吃
 $sql = "SELECT date,kind,food,pay,place,dmoney,takemoney,people,inviteurl FROM delivery where kind='小吃'";
 // 送出查詢的SQL指令
 if ( $result = mysqli_query($link, $sql) ) { 
@@ -59,34 +33,7 @@ while ($row = mysqli_fetch_row($result)) {
    echo "</tr>";
 }
 echo "</table>";
-        
-else(echo"查無資料,快去開起第一團吧";)
-        
-// 查詢日式
-$sql = "SELECT date,kind,food,pay,place,dmoney,takemoney,people,inviteurl FROM delivery where kind='日式'";
-// 送出查詢的SQL指令
-if ( $result = mysqli_query($link, $sql) ) { 
-   // 取得記錄數
-   $total_records = mysqli_num_rows($result);
-   echo "項目類別:日式<br/>"; 
-   echo "開團總數:共 $total_records 團<br/>"; 
-   echo "<table border=><tr>";
-// 顯示欄位名稱
-while ( $meta = mysqli_fetch_field($result) )
 
-   echo "<td>".$meta->name."</td>";
-echo "</tr>"; // 取得欄位數
-$total_fields = mysqli_num_fields($result);
-// 顯示每一筆記錄
-while ($row = mysqli_fetch_row($result)) {
-   echo "<tr>"; // 顯示每一筆記錄的欄位值
-   for ( $i = 0; $i <= $total_fields-1; $i++ )
-      echo "<td>" . $row[$i] . "</td>";
-   echo "</tr>";
-}
-echo "</table>";
-        
-else(echo"查無資料,快去開起第一團吧";)
    mysqli_free_result($result); // 釋放佔用記憶體 
 } 
 mysqli_close($link);  // 關閉資料庫連接

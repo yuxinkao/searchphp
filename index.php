a3c4ccc9245f640b13fdd86a9fef16528becd7d9
yuxinkao <99954219+yuxinkao@users.noreply.github.com>
2022-03-21 00:38:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,6 @@ $link = mysqli_connect("acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.co
 mysqli_select_db($link, "gk4xqozmcqv07zee");  // 選擇gk4xqozmcqv07zeev資料庫
 // 查詢台式
 $sql = "SELECT date,kind,food,pay,place,dmoney,takemoney,people,inviteurl FROM delivery where kind='台式'";
-// 查詢小吃
-$sqltwo = "SELECT date,kind,food,pay,place,dmoney,takemoney,people,inviteurl FROM delivery where kind='小吃'";
 // 送出查詢台式的SQL指令
 if ( $result = mysqli_query($link, $sql) ) { 
    // 取得記錄數
@@ -22,9 +20,10 @@ if ( $result = mysqli_query($link, $sql) ) {
    echo "開團總數:共 $total_records 團<br/>"; 
    echo "<table border=><tr>";
 // 顯示欄位名稱
-while ( $meta = mysqli_fetch_field($result) )
+// while ( $meta = mysqli_fetch_field($result) )
 
-   echo "<td>".$meta->name."</td>";
+   //echo "<td>".$meta->name."</td>";
+     echo "<td>."開團日期".."類型".."店家名稱".."付費方式".."取貨地點".."原外送費用".."外送接棒收費".."預計開放人數".."入團連結".</td>";
 echo "</tr>"; // 取得欄位數
 $total_fields = mysqli_num_fields($result);
 // 顯示每一筆記錄
@@ -35,31 +34,8 @@ while ($row = mysqli_fetch_row($result)) {
    echo "</tr>";
 }
 echo "</table>";
-        
-// 送出查詢小吃的SQL指令
-if ( $resulttwo = mysqli_query($link, $sqltwo) ) { 
-   // 取得記錄數
-   $total_recordstwo = mysqli_num_rows($resulttwo);
-   echo "項目類別:小吃<br/>"; 
-   echo "開團總數:共 $total_recordstwo 團<br/>"; 
-   echo "<table border=><tr>";
-// 顯示欄位名稱
-while ( $metatwo = mysqli_fetch_field($resulttwo) )
-
-   echo "<td>".$metatwo->name."</td>";
-echo "</tr>"; // 取得欄位數
-$total_fieldstwo = mysqli_num_fields($resulttwo);
-// 顯示每一筆記錄
-while ($rowtwo = mysqli_fetch_row($resulttwo)) {
-   echo "<tr>"; // 顯示每一筆記錄的欄位值
-   for ( $i = 0; $i <= $total_fieldstwo-1; $i++ )
-      echo "<td>" . $rowtwo[$i] . "</td>";
-   echo "</tr>";
-}
-echo "</table>";
-
+ 
    mysqli_free_result($result); // 釋放佔用記憶體 
-   mysqli_free_result($resulttwo); // 釋放佔用記憶體
 } 
 mysqli_close($link);  // 關閉資料庫連接
 ?>

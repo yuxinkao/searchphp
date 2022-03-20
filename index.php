d05e66e2d3c4306346bd6f77a0b6f4f9b27bb90f
yuxinkao <99954219+yuxinkao@users.noreply.github.com>
2022-03-20 23:48:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,17 +17,20 @@ if ( $result = mysqli_query($link, $sql) ) {
    // 取得記錄數
    $total_records = mysqli_num_rows($result);
    echo "開團總數:共 $total_records 團<br/>"; 
-   echo "<table border=1>";
-   echo "<tr><td>欄位名稱</td><td>資料表</td>";
-   echo "<td>最大長度</td><td>資料類型</td></tr>";
-   // 顯示欄位資訊
-   while ( $meta = mysqli_fetch_field($result) ) {
-      echo "<tr><td>" . $meta->name . "</td>";
-      echo "<td>" . $meta->table . "</td>";
-      echo "<td>" . $meta->max_length . "</td>";   
-      echo "<td>" . $meta->type . "</td></tr>";   
-   }
-   echo "</table>";   
+   echo "<table border=1><tr>";
+// 顯示欄位名稱
+while ( $meta = mysqli_fetch_field($result) )
+   echo "<td>".$meta->name."</td>";
+echo "</tr>"; // 取得欄位數
+$total_fields = mysqli_num_fields($result);
+// 顯示每一筆記錄
+while ($row = mysqli_fetch_row($result)) {
+   echo "<tr>"; // 顯示每一筆記錄的欄位值
+   for ( $i = 0; $i <= $total_fields-1; $i++ )
+      echo "<td>" . $row[$i] . "</td>";
+   echo "</tr>";
+}
+echo "</table>";
    mysqli_free_result($result); // 釋放佔用記憶體 
 } 
 mysqli_close($link);  // 關閉資料庫連接
